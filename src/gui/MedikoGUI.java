f7f631e4283d0c13b236dfae23d5e52e6936e4f5
Osmancan-Kucdemir <24181616013@gazi.edu.tr>
2025-12-20 15:24:13 +0300

    osmancan mediko

---
+++ src/gui/MedikoGUI.java
@@ -0,0 +1,34 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+public class MedikoGUI extends JFrame {
+    public MedikoGUI() {
+        setTitle("Mediko Sağlık Merkezi");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        String[] kolonlar = {"Bölüm", "Doktor Adı", "Uzmanlık", "Çalışma Günleri"};
+        DefaultTableModel model = new DefaultTableModel(kolonlar, 0);
+        JTable tablo = new JTable(model);
+
+        MedikoServisi servis = new MedikoServisi();
+        for (Map.Entry<String, List<Doktor>> entry : servis.getBolumler().entrySet()) {
+            for (Doktor d : entry.getValue()) {
+                model.addRow(new Object[]{
+                        entry.getKey(),
+                        d.tamAdGetir(),
+                        d.getUzmanlikAlani(),
+                        String.join(", ", d.getCalismaGunleri())
+                });
+            }
+        }
+
+        add(new JScrollPane(tablo), BorderLayout.CENTER);
+    }
+}
